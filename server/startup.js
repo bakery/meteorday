be226e11a7e055dac23f7cf87afbe7b282278a30
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-19 02:41:56 +0200

    add support for facebook login

--- server/startup.js
+++ server/startup.js
@@ -5,9 +5,19 @@ Meteor.startup(function(){
         service : 'twitter'
     });
 
+    Accounts.loginServiceConfiguration.remove({
+        service : 'facebook'
+    });
+
     Accounts.loginServiceConfiguration.insert({
         service     : 'twitter',
         consumerKey : Meteor.settings.private.twitter.consumerKey,
         secret      : Meteor.settings.private.twitter.secret
+    });
+
+    Accounts.loginServiceConfiguration.insert({
+        service     : 'facebook',
+        appId 		: Meteor.settings.private.facebook.applicationId,
+        secret      : Meteor.settings.private.facebook.applicationSecret
     });
 });
--- server/users.js
+++ server/users.js
@@ -1,6 +1,22 @@
 Accounts.onCreateUser(function(options, user){
-    var extendedProfile = _.extend(options.profile || {}, {
-        imageUrl : user.services.twitter.profile_image_url_https
-    });
+
+    var extendProfileWith = {};
+
+    if(user.services.twitter){
+        extendedProfileWith = {
+            imageUrl : user.services.twitter.profile_image_url_https
+        };
+    }
+
+    if(user.services.facebook){
+        extendProfileWith = {
+            imageUrl : [
+                'http://graph.facebook.com', user.services.facebook.id, 'picture'
+            ].join('/')
+        };
+    }
+
+    var extendedProfile = _.extend(options.profile || {}, extendProfileWith);
+
     return _.extend(user, { profile : extendedProfile });
 });
