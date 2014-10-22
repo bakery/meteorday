4a2a245baffaee9893124b5ade804497dcea0356
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-22 20:50:11 +0200

    configure cordova facebook plugin

--- mobile-config.js
+++ mobile-config.js
@@ -1,8 +1,10 @@
 // Set PhoneGap/Cordova preferences
 App.setPreference('StatusBarOverlaysWebView', false);
 
+//885250964821245
+
 // Pass preferences for a particular PhoneGap/Cordova plugin
 App.configurePlugin('https://github.com/Wizcorp/phonegap-facebook-plugin.git', {
-  APP_ID: 885250964821245,
+  APP_ID: 726812950734516,
   APP_NAME: 'Waldof'
 });
