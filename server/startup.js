735394179b6a1953af0ccdfd0a2ada294d31dc09
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-24 18:57:25 +0200

    remove twitter oauth setup

--- server/startup.js
+++ server/startup.js
@@ -1,18 +1,6 @@
 Meteor.startup(function(){
-    // enable Twitter OAuth
-
-    Accounts.loginServiceConfiguration.remove({
-        service : 'twitter'
-    });
-
     Accounts.loginServiceConfiguration.remove({
         service : 'facebook'
-    });
-
-    Accounts.loginServiceConfiguration.insert({
-        service     : 'twitter',
-        consumerKey : Meteor.settings.private.twitter.consumerKey,
-        secret      : Meteor.settings.private.twitter.secret
     });
 
     Accounts.loginServiceConfiguration.insert({
