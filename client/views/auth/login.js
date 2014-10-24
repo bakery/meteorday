418e35be3a421fab0b90a65126c726c1e0ad8a90
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-24 18:58:21 +0200

    remove twitter login

--- client/views/auth/login.js
+++ client/views/auth/login.js
@@ -1,9 +1,4 @@
 Template.login.events({
-    'click .login-twitter' : function () {
-        Meteor.loginWithTwitter(function(err){
-            //handle error;
-        });
-    },
     'click .login-facebook' : function () {
         Meteor.loginWithFacebook(function(err){
             //handle facebook
