6767794cd4f998fb1588dc579954b3603d4a6584
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-29 22:31:30 +0100

    add modal location selector

---
+++ client/views/checkins/location-selector.js
@@ -0,0 +1,21 @@
+Template.locationSelector.helpers({
+    isSelected : function(){
+        return LocationSession.getSelectedLocation() === this.id;
+    }
+});
+
+Template.locationSelector.events({
+    'click .location' : function(e, template){
+        var selectedId = $(e.currentTarget).data('id');
+        if(selectedId){
+            LocationSession.setSelectedLocation(selectedId);
+
+            var modal = template.$('.location-selector-modal');
+
+            // XXX check checkins/form.js for explanations
+            var restore = modal.data('target');
+            $(restore).show();
+            modal.removeClass('active');
+        }
+    }
+});
