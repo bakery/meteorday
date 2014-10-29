0c34e6b9a6ca6c0a3c3f00b3d4004c182deb64a5
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-29 23:26:49 +0100

    dynamically update item time stamp label

--- client/views/checkins/item.js
+++ client/views/checkins/item.js
@@ -1,5 +1,11 @@
+var timeRightNow = new ReactiveVar(new Date());
+
+setInterval(function(){
+    timeRightNow.set(new Date());
+},1000);
+
 Template.item.helpers({
     prettyCheckinTime : function(){
-        return moment(this.created).fromNow();
+        return moment(this.created).from(timeRightNow.get());
     }
 });
