0fc2311badadb352caaeb2293fb3f3b6eff7a5bc
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-31 20:42:56 +0100

    add currentUserProfile helper

--- client/views/checkins/checkin_header.js
+++ client/views/checkins/checkin_header.js
@@ -1,9 +1,12 @@
 Template.checkinHeader.helpers({
-    
     isFormExpanded : function(){
         return Session.get('checkin-form-expanded');
-    }
+    },
 
+    currentUserProfile : function(){
+        var user = Meteor.user();
+        return user ? user.profile : null;
+    }
 });
 
 Template.checkinHeader.events({
