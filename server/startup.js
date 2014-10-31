961776f736d2b1287cb40b1ecbbc3ff6ab84f961
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-10-31 19:08:46 +0100

    add meteor login

--- server/startup.js
+++ server/startup.js
@@ -5,7 +5,17 @@ Meteor.startup(function(){
 
     Accounts.loginServiceConfiguration.insert({
         service     : 'facebook',
-        appId 		: Meteor.settings.private.facebook.applicationId,
+        appId       : Meteor.settings.private.facebook.applicationId,
         secret      : Meteor.settings.private.facebook.applicationSecret
+    });
+
+    Accounts.loginServiceConfiguration.remove({
+        service : 'meteor-developer'
+    });
+
+    Accounts.loginServiceConfiguration.insert({
+        service     : 'meteor-developer',
+        clientId    : Meteor.settings.private.meteor.clientId,
+        secret      : Meteor.settings.private.meteor.secret
     });
 });
