3e23733b5e49dd6eb2528fb759d5b34d90223c0c
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-11-03 20:28:10 +0100

    add a helper to get a random user

--- server/lib/tools.js
+++ server/lib/tools.js
@@ -4,5 +4,9 @@ Tools = {
     },
     getRandomInt : function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    getRandomUser : function(){
+        var users = Meteor.users.find({}).fetch();
+        return users[this.getRandomInt(0,users.length-1)];
     }
 };
